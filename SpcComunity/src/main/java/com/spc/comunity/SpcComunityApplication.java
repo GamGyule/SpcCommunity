a852f50461a005368cbefd189dc29c35b7c7e41b
GamGyul <gamgyule@kakao.com>
2022-03-25 00:06:40 +0900

    초기 세팅
    SpringBoot + JPA
    
    todo-
    vue 프론트

---
+++ SpcComunity/src/main/java/com/spc/comunity/SpcComunityApplication.java
@@ -0,0 +1,13 @@
+package com.spc.comunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpcComunityApplication.class, args);
+	}
+
+}
