78c7bef1c01b11a020ba95535481a04cdd543dec
GamGyul <gamgyule@kakao.com>
2022-03-31 00:13:03 +0900

    헤더,푸터, 지갑연결테스트, 백엔드테스트, 페이지 이동

---
+++ SpcComunity/frontend/store/store.js
@@ -0,0 +1,16 @@
+import create from 'zustand'
+import { devtools } from 'zustand/middleware'
+import axios from 'axios';
+
+export const useStore = create(devtools((set) => ({
+    walletAddress: "",
+    backData: "",
+
+    //ajax 요청법
+    async BackConnectApi() {
+        axios.get('/api/hi').then(function (e) {
+            set((state) => ({ backData: e.data }))
+        });
+    }
+
+})))
