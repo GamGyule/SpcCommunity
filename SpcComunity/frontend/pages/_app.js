7fb27f8907530b44d793735bd637c34a25fb92b1
sajo <sajo@deleo.co.kr>
2022-04-28 17:43:41 +0900

    react-bootstrap import 위치 변경

--- SpcComunity/frontend/pages/_app.js
+++ SpcComunity/frontend/pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.scss'
 
 // 사용할 컴포넌트가 있다면 아래와 같은 식으로 컴포넌트 붙여주면 <Button>BUTTON</Button> 이런코드 사용가능
 // import { 사용할 컴포넌트명 } from 'react-bootstrap';
