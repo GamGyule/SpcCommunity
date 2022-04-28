7da660ff4b5c58e2b7d0fdcba461cdb29045d9cb
sajo <sajo@deleo.co.kr>
2022-04-28 17:29:38 +0900

    react-bootstrap 설치

--- SpcComunity/frontend/pages/_app.js
+++ SpcComunity/frontend/pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 사용할 컴포넌트가 있다면 아래와 같은 식으로 컴포넌트 붙여주면 <Button>BUTTON</Button> 이런코드 사용가능
+// import { 사용할 컴포넌트명 } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
