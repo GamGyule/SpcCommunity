8533a280dea97c6bcb06e21ecc658d4f6d90cd91
SU A JO <>
2022-05-28 21:04:51 +0900

    footer 스타일 수정, body background 퍼센트 수정

--- SpcComunity/frontend/components/public/Footer.js
+++ SpcComunity/frontend/components/public/Footer.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Logo from '../../public/logo.svg'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Stack} from 'react-bootstrap'
 import Styles from '../../styles/Footer.module.scss'
 import IconDiscord from '../../public/icons/icon-discord.svg'
 import IonPP from '../../public/icons/icon-pp.svg'
@@ -10,19 +10,19 @@ import IconTop from '../../public/icons/icon-btn-top.svg'
 
 const Footer = () => {
     return (
-        <Container fluid className={`${Styles.FooterWrap}`}>
-            <Row className={`${Styles.FooterBox} mt-4`}>
+        <Container fluid className={`${Styles.FooterWrap} mt-5`}>
+            <Row className={`${Styles.FooterBox} p-3`}>
                 <Col xs={12} className={`${Styles.FooterBar} d-flex align-items-center justify-content-between`}>
-                    <div>
+                    <div className="d-flex flex-column align-items-start">
                         <Image src={Logo} alt="logo"></Image>
-                        Copyright © POOPLELA All right reserved.. 
+                        <p className="mt-1 mb-0 small text-white">Copyright © POOPLELA All right reserved..</p> 
                     </div>
-                    <div className="d-flex align-items-center">
+                    <Stack direction="horizontal" gap={3} className="d-flex align-items-center">
                         <Link href="/"><Image src={IconDiscord} alt="Discord"></Image></Link>
                         <Link href="/"><Image src={IonPP} alt="PP"></Image></Link>
                         <Link href="/"><Image src={IconTwitter} alt="Twitter"></Image></Link>
                         <Link href="/"><Image src={IconTop} alt="Top"></Image></Link>
-                    </div>
+                    </Stack>
                 </Col>
             </Row>
         </Container>
